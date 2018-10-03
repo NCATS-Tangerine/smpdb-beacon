7e05956375a3baf4d99b6cedebfaf4a38b59c457
Richard Bruskiewich <richard@starinformatics.com>
2018-10-03 21:00:36 +0000

    Broken commands up in concat_metabolites.sh

--- data/concat_metabolites.sh
+++ data/concat_metabolites.sh
@@ -1,2 +1,14 @@
+# Here I've broken up the concatenation commands because otherwise there are too many inputs
+
 head -1 metabolites/SMP0028016_metabolites.csv > metabolites.csv
-for filename in $(ls metabolites/SMP*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP000*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP001*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP002*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP003*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP004*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP005*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP006*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP007*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP008*.csv); do sed 1d $filename >> metabolites.csv; done
+for filename in $(ls metabolites/SMP009*.csv); do sed 1d $filename >> metabolites.csv; done
+
