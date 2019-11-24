63a97039fe3a831452d401b6271412a52f0ff8f7
Lance Hannestad <lancehannestad@gmail.com>
2019-11-24 08:22:00 -0800

    Fixed depricated use of yaml.load

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,9 @@
-import os, yaml
+import os
+import yaml
+
+from yaml import FullLoader
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yaml')
 
 with open(path, 'r') as f:
-    config = yaml.load(f)
+    config = yaml.load(f, Loader=FullLoader)
